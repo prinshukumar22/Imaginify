6cfd26ea102861d281e47e5b346a1d47ec01901e
Prinshu <prinshu2003@gmail.com>
2024-03-26 00:40:02 +0530

    checked something

--- app/components/shared/Sidebar.tsx
+++ app/components/shared/Sidebar.tsx
@@ -23,7 +23,7 @@ const Sidebar = () => {
         <nav className="sidebar-nav">
           <SignedIn>
             <ul className="sidebar-nav_elements">
-              {navLinks.slice(0, 6).map((link) => {
+              {navLinks.slice(0,6).map((link) => {
                 const isActive = link.route === pathname;
                 return (
                   <li
